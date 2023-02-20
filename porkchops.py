e91882ab1a3e3ca4653fc365bccc2a9edf9bce02
UnicornLover975 <124210262+UnicornLover975@users.noreply.github.com>
2023-02-20 13:10:17 +1300

    Add files via upload

---
+++ porkchops.py
@@ -0,0 +1,9 @@
+want_instructions = input("do you want to read the instructions??????? ")
+print(want_instructions)
+
+if want_instructions == "yes":
+    print("instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("please answer yes/no")
