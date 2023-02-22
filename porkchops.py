6f79e72a42b4d5296eb52682274b83f5fcb355df
UnicornLover975 <124210262+UnicornLover975@users.noreply.github.com>
2023-02-23 10:41:11 +1300

    Add files via upload

--- porkchops.py
+++ porkchops.py
@@ -1,9 +1,27 @@
-want_instructions = input("do you want to read the instructions??????? ")
-print(want_instructions)
+#functions go here
+def yes_no(question):
 
-if want_instructions == "yes":
-    print("instructions go here")
-elif want_instructions == "no":
-    pass
-else:
-    print("please answer yes/no")
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("please answer yes/no")
+
+#main routine goes here
+
+while True:
+    want_instructions = yes_no("do you want to read the instructions??????? ")
+    print(want_instructions)
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("instructions go here")
+    elif want_instructions == "no" or want_instructions == "n":
+        print("program continues")
+    else:
+        print("please answer yes/no")
+
+print("We are done.")
